9e42c3c267179a990bb66e31d23f981ebea7d6e6
David Sevilla <sevilladavid83@gmail.com>
2025-02-08 16:28:50 -0500

    Use mini.icons

--- .config/nvim/lua/default/plugins/mini.lua
+++ .config/nvim/lua/default/plugins/mini.lua
@@ -4,5 +4,7 @@ return {
 		require("mini.ai").setup({ n_lines = 500 })
 		require("mini.surround").setup()
 		require("mini.pairs").setup()
+		require("mini.icons").setup()
+		MiniIcons.mock_nvim_web_devicons()
 	end,
 }
