495b7b1af28cb3ba866de3a8a8e2477f7e73f0e4
David Sevilla <sevilladavid83@gmail.com>
2025-02-17 21:55:52 -0500

    Add back mini.pairs

--- .config/nvim/lua/default/plugins/mini.lua
+++ .config/nvim/lua/default/plugins/mini.lua
@@ -2,6 +2,7 @@ return {
 	"echasnovski/mini.nvim",
 	config = function()
 		require("mini.ai").setup({ n_lines = 500 })
+		require("mini.pairs").setup({})
 		require("mini.surround").setup({})
 		vim.keymap.set({ "n", "x" }, "s", "<Nop>")
 
